18ae895956c469af744076264ec6a9385abce483
Lucas Emanuel Perim Martins <lucasepmartins2008@gmail.com>
2025-01-07 18:16:52 -0300

    lala

---
+++ numeros.py
@@ -0,0 +1,10 @@
+num1 = float(input("qual é o primeiro número"))
+num2 = float(input("qual é o segundo número"))
+soma = num1 + num2
+subtração = num1 - num2
+multiplicação = num1 * num2
+divisão = num1/num2
+print(soma)
+print(subtração)
+print(multiplicação)
+print(divisão)
