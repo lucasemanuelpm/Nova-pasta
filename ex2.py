26fc6917b72f3bdf8748dcc5c27cc3aafcf83bb6
Lucas Emanuel Perim Martins <lucasepmartins2008@gmail.com>
2025-01-07 17:33:37 -0300

    .

--- ex2.py
+++ ex2.py
@@ -1,11 +1,8 @@
 nome = input ("qual é o seu nome?")
-print("Bem vindo " , nome )
 altura = input ("qual é a sua altura")
-print( "sua altura é" , altura )
 idade = input ("qual é a sua idade?")
-print("a sua idade é " , idade )
 estudante = input("você é estudante?")
-if estudante == "sim":
-    print("Ótimo! Continue estudando.")
-else:
-    print("poxa que pena estudar é sempre bom") 
+print("Bem vindo " , nome )
+print("a sua idade é " , idade )
+print( "sua altura é" , altura )
+print(estudante)
