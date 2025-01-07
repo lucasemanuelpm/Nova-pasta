7b9d58852c2cdc16bb8ec47fd9e8321d63d07809
Lucas Emanuel Perim Martins <lucasepmartins2008@gmail.com>
2025-01-07 17:55:30 -0300

    la

--- ex2.py
+++ ex2.py
@@ -6,3 +6,9 @@ print("Bem vindo " , nome )
 print("a sua idade é " , idade )
 print( "sua altura é" , altura )
 print(estudante)
+ano_denascimento = 2024 - idade
+print(ano_denascimento)
+if idade < 18 :
+ print("voce é maior de idade")
+else: 
+ print("você não é maior de idade")
