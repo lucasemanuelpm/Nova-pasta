44784b5f52f1a080239e04fe067904fbf04f60c7
Lucas Emanuel Perim Martins <lucasepmartins2008@gmail.com>
2025-01-07 18:04:03 -0300

    aaa

--- p.py
+++ p.py
@@ -1,2 +1,3 @@
-idade = input("qual a sua idade?")
-anode_nascimento = int((2024 - idade))
+idade = int(input("qual a sua idade?"))
+anode_nascimento = 2024 - idade
+print("seu ano de nascimento é", anode_nascimento)
