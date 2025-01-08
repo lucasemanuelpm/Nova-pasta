1e332e53300e8f618054f3488f73f71ded3015c0
Lucas Emanuel Perim Martins <lucasepmartins2008@gmail.com>
2025-01-08 19:07:32 -0300

    a

---
+++ dia3.py
@@ -0,0 +1,19 @@
+#operadores aritmeticos
+a = 4
+b = 6
+print( a + b)
+print(a - b)
+print(a * b)
+print(a/b)
+# ** - exponenciação
+# // divisão inteira
+ 
+print(a//b)
+print(a%b)
+
+#operadores de comparação
+x = 5
+y = 4
+print(x<y)
+print(x>y)
+print(x!=y)
