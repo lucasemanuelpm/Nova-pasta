f4e585040d537ca7a0e8e97952c841695fd98829
Lucas Emanuel Perim Martins <lucasepmartins2008@gmail.com>
2025-01-09 10:46:12 -0300

    a

---
+++ meiaentradaifeelse.py
@@ -0,0 +1,6 @@
+idade = 60
+estudante = False
+if idade or estudante == idade>=60 or estudante == True :
+    print("otimo voce esta apto a meia entrada")
+else : 
+    print("que pena você não esta apto a meia entrada")
