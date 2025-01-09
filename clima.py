7b0981596eed11c9d9844be94b9943e0d99c4043
Lucas Emanuel Perim Martins <lucasepmartins2008@gmail.com>
2025-01-09 12:00:57 -0300

    a

---
+++ clima.py
@@ -0,0 +1,8 @@
+#clima ensolarado nublado e chuvoso
+clima = str(input("como esta o clima hoje?"))
+if clima == "ensolarado" :
+    print("aproveite o dia")
+elif clima == "nublado" :
+    print("pratique atividades fisicas dentro de casa")
+elif clima == "chuvoso" :
+    print("assista file ou serie")
