c9f83e2cf5dec016ed053fb83b44a7d1a7ab2700
Lucas Emanuel Perim Martins <lucasepmartins2008@gmail.com>
2025-01-09 11:27:23 -0300

    aaa

--- nota.py
+++ nota.py
@@ -1,7 +1,6 @@
 #nota A;B;C
 # >9 =A >8 = B >7 = C
 nota_exata = input("sua nota foi exata?")
-nota_inexata = input("sua nota foi inexata?") 
 if nota_exata :
  nota = int(input("qual foi a sua nota?"))
 if nota>=9 :
@@ -10,14 +9,16 @@ elif nota>=8 :
     print("uau meus parabens voce tirou um B")
 elif nota>=7 :
     print("uau meus parabens voce tirou um C")   
+nota_inexata = input("sua nota foi inexata")
 if nota_inexata :
- nota = float(input("qual foi a sua nota?"))
+       nota = float(input("qual foi a sua nota?"))
 if nota>=9 :
     print("uau meus parabens voce tirou um A")
 elif nota>=8 :
     print("uau meus parabens voce tirou um B")
 elif nota>=7 :
-    print("uau meus parabens voce tirou um C")
+    print("uau meus parabens voce tirou um C")  
+
 
 
    
