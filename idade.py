47c0939f351ed7fb92fc4f2be7f9f2e321435f52
Lucas Emanuel Perim Martins <lucasepmartins2008@gmail.com>
2025-01-09 15:07:33 -0300

    aa

---
+++ idade.py
@@ -0,0 +1,9 @@
+idade = int(input("qual a sua idade?"))
+if idade <= 12:
+ print("você é uma criança")
+elif idade >=13 and idade <=17:
+ print("você é um adolescente")
+elif idade >=18 and idade <=59 :
+ print("você é um adulto")
+elif idade  >=60 :
+ print("você é um idoso")
