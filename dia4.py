ed0d4832a8707411d6607eb30519e5012106100a
Lucas Emanuel Perim Martins <lucasepmartins2008@gmail.com>
2025-01-09 10:32:44 -0300

    a

--- dia4.py
+++ dia4.py
@@ -1,4 +1,8 @@
 #if verifica se uma condição é verdadeira
 #elif permite verificar múltiplas condições sequencialmente
 #else é utilizado para executar um bloco de código, caso o resultado da expressão informada na instrução if seja falso.
- 
+idade = 18
+if idade == (idade >=18) :
+ print("otimo voce é maior de idade")
+else :
+ print("vc ainda nao é maior de idade")
