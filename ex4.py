0ba9e4bd6ea1643cc071b8daa621d99792345ad1
Lucas Emanuel Perim Martins <lucasepmartins2008@gmail.com>
2025-01-09 12:50:16 -0300

    aaa

---
+++ ex4.py
@@ -0,0 +1,7 @@
+num = int(input("Digite um numero"))
+if num ==0 :
+    print("o número que voce escolheu é zero")
+elif num >0 :
+    print("então o número que voce escolheu é positivo")
+elif num <0 :
+    print("então o número que voce escolheu é negativo")
