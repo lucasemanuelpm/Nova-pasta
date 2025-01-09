b79f10382479acc331c68e06ef541d32e9a1326f
Lucas Emanuel Perim Martins <lucasepmartins2008@gmail.com>
2025-01-09 10:58:58 -0300

    a

---
+++ nota.py
@@ -0,0 +1,9 @@
+#nota A;B;C
+# >9 =A >8 = B >7 = C
+nota = int(input("qual foi a sua nota?"))
+if nota>9 :
+    print("uau meus parabens voce tirou um A")
+elif nota>8 :
+    print("uau meus parabens voce tirou um B")
+elif nota>7 :
+    print("uau meus parabens voce tirou um C")
