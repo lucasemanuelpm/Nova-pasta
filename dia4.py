61fd8b7a9dc251b9ae3c2e157f7a8e6025c27108
Lucas Emanuel Perim Martins <lucasepmartins2008@gmail.com>
2025-01-09 10:28:58 -0300

    a

--- dia4.py
+++ dia4.py
@@ -1,1 +1,4 @@
 #if verifica se uma condição é verdadeira
+#elif permite verificar múltiplas condições sequencialmente
+#else é utilizado para executar um bloco de código, caso o resultado da expressão informada na instrução if seja falso.
+ 
