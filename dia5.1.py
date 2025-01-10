b6eae405d068c04f59c009c12831a4445293fcfa
Lucas Emanuel Perim Martins <lucasepmartins2008@gmail.com>
2025-01-10 11:09:34 -0300

    aaaa

--- dia5.1.py
+++ dia5.1.py
@@ -1,7 +1,10 @@
+numero = int(input("qual o numero voce deseja"))
 pares = 0
 impares = 0 
 for i in range( 1, 21) :
     if i %2 == 0 :
         print("o numero é par")
+        pares =+ 1
     elif i%3 == 0 :
         print("o numero é impar")
+        impares +1
