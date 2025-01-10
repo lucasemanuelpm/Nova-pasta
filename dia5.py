2724d297c8fd12ee9054d324d47be097988bad9d
Lucas Emanuel Perim Martins <lucasepmartins2008@gmail.com>
2025-01-10 10:21:32 -0300

    aaa

--- dia5.py
+++ dia5.py
@@ -1,4 +1,8 @@
 #estrutura de repetição
 #Repetir N vezes onde quem define é o usuario ou o programador
 #ou n é uma condição
+#For item_individual in lista
+# bloco repeitdo N vezes
 frutas = ("maça", "laranja", "banana")
+for fruta in frutas :
+ print("Fruta:", frutas)
