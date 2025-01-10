645510b2a1de42ac382fa1ea3f7b5a8c0bc8be71
Lucas Emanuel Perim Martins <lucasepmartins2008@gmail.com>
2025-01-10 11:14:08 -0300

    aaa

--- dia5.1.py
+++ dia5.1.py
@@ -1,10 +1,9 @@
-numero = int(input("qual o numero voce deseja"))
 pares = 0
-impares = 0 
-for i in range( 1, 21) :
-    if i %2 == 0 :
-        print("o numero é par")
-        pares =+ 1
-    elif i%3 == 0 :
-        print("o numero é impar")
-        impares +1
+impares = 0
+for numero in range(1, 21):
+ if numero % 2 == 0:
+  pares += 1
+else:
+ impares += 1
+print("Quantidade de números pares:", pares)
+print("Quantidade de números ímpares:", impares)
