3920ef38617bbc68ace26dfc6402e096c324547a
Lucas Emanuel Perim Martins <lucasepmartins2008@gmail.com>
2025-01-10 10:09:14 -0300

    aaa

--- dia2.3.py
+++ dia2.3.py
@@ -1,4 +1,3 @@
 Celcius = float(input("qual a temperatura em celcius"))
 fahrenheit =  Celcius * 9/5 + 32
 print("a temperatura em Fahreint é ", fahrenheit)
-
---
+++ p.py
@@ -0,0 +1,4 @@
+idade = 16
+nome = "lucas"
+frase = ("ola meu nome é"+ nome + "minha idade é" + str(idade))
+print(frase)
